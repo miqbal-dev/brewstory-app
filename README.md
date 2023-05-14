8938ddfe39c304d17e47bbf7c7aa3779ab3747ce
ardodeardo <simadanaga@gmail.com>
2023-05-15 00:51:49 +0700

    chore: update readme

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
+<div align="center">
+  <h1 align="center" style="font-weight:bold">B R E W S T O R Y</h1>
+  <p align="center">
+    A blog prototyping material
+    <br /><br />
+    <img src="https://github.com/ardodeardo/brewstory-app/assets/61144146/82e0f576-7528-4578-861b-e295732dcdd3">
+    <br />
+    <a href="https://brewstory-app.vercel.app/">View Demo</a>
+    <br /><br />
+  </p>
+</div>
+
+
+## About The Project
+Hi there!
+
+This is another of my prototyping material, the design itself is a re-create of [overreacted.io](https://overreacted.io/) blog by the legend of React JS creator Dan Abramov.
+
+Checkout the design on my [figma.com/@ardodeardo](https://www.figma.com/@ardodeardo) :v:
+## Built With
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+
+## Quick tour of the structure
+
+```
+brewstory-app
+└── public
+└── app
+  ├── components
+  ├── constants
+  ├── helper
+  ├── interfaces
+  ├── read/[id]
+  ├── styles
+  ├── uikit
+  ├── globals.scss
+  ├── layout.tsx
+  └── page.tsx
+```
 
 ## Getting Started
 
@@ -13,22 +51,3 @@ pnpm dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
--- app/read/[id]/page.tsx
+++ app/read/[id]/page.tsx
@@ -11,22 +11,21 @@ function Read(props: any) {
           <time dateTime={`February 5, 2023`} className='text-sm mt-1'>{`February 5, 2023`}</time>
           <div className='o-wysiwyg mt-6 md:mt-10'>
             <p>
-              If you're a coffee drinker, you've probably wondered what the difference is between a latte and a cappuccino. The answer is simple: not much! Both drinks are made with steamed milk and espresso, but they differ in how they're prepared.
+              If you&apos;re a coffee drinker, you&apos;ve probably wondered what the difference is between a latte and a cappuccino. The answer is simple: not much! Both drinks are made with steamed milk and espresso, but they differ in how they&apos;re prepared.
             </p>
             <br />
-            <p>Latte is one of the most popular drinks at cafes and coffee shops. It's made with espresso and steamed milk, which is poured over a layer of foam. The foam should be thick enough to hold its shape but not so thick that it doesn't mix easily with your drink.
+            <p>Latte is one of the most popular drinks at cafes and coffee shops. It&apos;s made with espresso and steamed milk, which is poured over a layer of foam. The foam should be thick enough to hold its shape but not so thick that it doesn&apos;t mix easily with your drink.
               The espresso should be strong enough to give you a boost, but not so strong that it knocks you out or makes your stomach hurt!</p>
             <img src="/images/nyc.jpg" alt="nyc" />
             <p>
-              A cappuccino is a latte with frothed milk and foam on top. The drink originated in Italy, where it was served at the Cappuccino coffee shop in Florence. It was named after the Capuchin order of friars who wore brown robes and hoods that resembled those of monks' cowls.
-              The key difference between a cappuccino and a latte is that while both drinks have espresso as their base, only the latter has steamed milk added to it--and then more foam! With this extra step involved in its creation, you might think that your barista will charge more money for your order; however this isn't always true: some cafes offer discounts if you buy multiple drinks at once (like getting five lattes instead of four).
+              A cappuccino is a latte with frothed milk and foam on top. The drink originated in Italy, where it was served at the Cappuccino coffee shop in Florence. It was named after the Capuchin order of friars who wore brown robes and hoods that resembled those of monks&apos; cowls.
+              The key difference between a cappuccino and a latte is that while both drinks have espresso as their base, only the latter has steamed milk added to it--and then more foam! With this extra step involved in its creation, you might think that your barista will charge more money for your order; however this isn&apos;t always true: some cafes offer discounts if you buy multiple drinks at once (like getting five lattes instead of four).
             </p>
             <br />
-            <p>A latte is coffee with steamed milk, while a cappucino is coffee with steamed milk and frothed milk. The difference between these two drinks is that in a latte you get more liquid than foam, while in a cappuccino there's more foam than liquid.</p>
+            <p>A latte is coffee with steamed milk, while a cappucino is coffee with steamed milk and frothed milk. The difference between these two drinks is that in a latte you get more liquid than foam, while in a cappuccino there&apos;s more foam than liquid.</p>
           </div>
         </article>
       </div>
-
     </>
   )
 }
